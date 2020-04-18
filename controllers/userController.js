d1bd24f303694d47bc8d4756f6b81cd2a06eb058
Diego Taveras <diegotaveras2@outlook.com>
2020-04-18 00:50:15 -0400

    Checking if the user exist

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,30 @@
+const User = require('../models/User')
 
+exports.createUser = async (req, res) => {
 
-exports.createUser = (req, res) => {
-    console.log(req.body)
+    // extract email and pasword
+    const {email,password}=req.body
+
+    
+    try {
+        let user = await User.findOne({email});
+
+        if(user){
+            return res.status(400).json({msg: 'User Exist'})
+        }
+
+        // create new user
+        user = new User(req.body);
+
+        // save user
+        await user.save()
+
+        // Confirmation message
+        res.json({msg: 'User Create corectly'})
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('and error ocured')
+    }
+
 }
