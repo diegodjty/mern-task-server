2b5de53f91b6656a6782052d34b3cde11808ba93
Diego Taveras <diegotaveras2@outlook.com>
2020-04-18 12:25:19 -0400

    add hash to password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
 
 exports.createUser = async (req, res) => {
 
@@ -15,6 +16,10 @@ exports.createUser = async (req, res) => {
 
         // create new user
         user = new User(req.body);
+
+        // Hash the password
+        const salt = await bcryptjs.genSalt(10);
+        user.password = await bcryptjs.hash(password,salt)
 
         // save user
         await user.save()
