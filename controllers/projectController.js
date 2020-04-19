a5414a031fd6bdb3e05e780042b54fe1a91aafbf
Diego Taveras <diegotaveras2@outlook.com>
2020-04-19 01:55:48 -0400

    validating project

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,14 @@
 const Project = require('../models/Project')
-
+const {validationResult} = require('express-validator')
+ 
 exports.createProjects = async (req, res)=>{
+
+
+    // Check for error
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(401).json({errors: errors.array()})
+    }
 
     try {
 
--- routes/projects.js
+++ routes/projects.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController')
 const auth = require('../middleware/auth')
+const {check} = require('express-validator')
 // Create projects
 //
 router.post('/',
     auth,
+    [
+        check('name','The name is required').not().isEmpty()
+    ],
     projectController.createProjects
 )
 router.get('/',
