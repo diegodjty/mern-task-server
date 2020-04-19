db5e424b2b3e955b3d4abd06bd990dbd3b9d0169
Diego Taveras <diegotaveras2@outlook.com>
2020-04-18 22:07:29 -0400

    Inserting new projects

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,4 +1,17 @@
+const Project = require('../models/Project')
 
 exports.createProjects = async (req, res)=>{
+
+    try {
+
+        // Create new project
+        const project = new Project(req.body)
+        project.save();
+        res.json(project)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('An error occured')
+    }
 
 }
